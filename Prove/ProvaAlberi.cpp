4a5e591034c36d5235fad78012b2c7dd15859602
Alex Fiorini <61363883+AlexFiorini@users.noreply.github.com>
2022-10-19 08:59:06 +0200

    Update ProvaAlberi.cpp

--- Prove/ProvaAlberi.cpp
+++ Prove/ProvaAlberi.cpp
@@ -1,35 +1,45 @@
 #include <stdio.h>
 #include <malloc.h>
 #define COUNT 10
+#define N 255
 
 typedef struct Nodo{
     int freq;	//Somma dei nodi nel nodo intermedio
-    char lett; //Non c'è nel nodo intermedio
+    char lett; //Non c'e^ nel nodo intermedio
     struct Nodo *sx;
     struct Nodo *dx;
 }Nodo;
 
 Nodo* Crea();
-Nodo* Inserisci();
-void swap(int* xp, int* yp);
-void selectionSort(Nodo* arr[], int n);
+void swap(Nodo* xp, Nodo* yp);
+void selectionSort();
 
 
 Nodo *radice;
-Nodo **p = new Nodo *[255];
-Nodo **inte = new Nodo *[128];
+Nodo **p = new Nodo *[N];
+//Nodo **inte = new Nodo *[128];
 
 int main()
 {   
     int i=0;
+	
+	for(i=0; i<N; i++)
+	{
+		p[i]=NULL;
+	}
 	
 	for(i=0; i<5; i++)
 	{
 		p[i]=Crea();
 	};
 	
-	selectionSort(p, 255);
+	selectionSort();
 	
+	for(i=0; i<N; i++)
+	{
+		if(p[i]!=NULL)
+		printf("%d\n", p[i]->freq);
+	}
 }
 
 Nodo* Crea()
@@ -43,39 +53,35 @@ Nodo* Crea()
 	scanf("%d", &p->freq);
 	return p;
 }
-
-Nodo* Inserisci()
-{
-	Nodo *p;
-	p=(Nodo*)malloc(sizeof(Nodo));
-	p->sx=NULL;
-	p->dx=NULL;
-	
-}
-
-void swap(int* xp, int* yp)
-{
-    int temp = *xp;
-    *xp = *yp;
-    *yp = temp;
-}
  
-// Function to perform Selection Sort
-void selectionSort(Nodo* arr[], int n)
+void selectionSort()
 {
     int i, j, min_idx;
- 
-    // One by one move boundary of unsorted subarray
-    for (i = 0; i < n - 1; i++) {
- 
-        // Find the minimum element in unsorted array
-        min_idx = i;
-        for (j = i + 1; j < n; j++)
-            if (p[j]->freq < p[min_idx]->freq)
-                min_idx = j;
- 
-        // Swap the found minimum element
-        // with the first element
-        swap(&arr[min_idx], &arr[i]);
+    
+    
+    for(i=0; i<N; i++) 
+	{
+		if(p[i]!=NULL)
+		{
+			// min_idx = posizione del minore
+	        min_idx=i;
+		
+			for(j=i+1; j<N; j++)
+	        {
+	        	if(p[j]!=NULL)
+	        	{
+	        		if(p[j]->freq < p[min_idx]->freq)
+		            {
+		            	min_idx=j;
+					}
+				}
+			}
+	        if(min_idx!=i)
+	        {
+	        	Nodo* temp = p[min_idx];
+				p[min_idx] =  p[i];
+				p[i] = temp;
+			}	
+		}
     }
 }
