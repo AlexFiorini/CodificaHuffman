b98c41429a4f6e0e4d124dda6f3d19ab5bc43b42
AlexFiorini <61363883+AlexFiorini@users.noreply.github.com>
2022-10-26 19:40:29 +0200

    Update ProvaAlberi.cpp
    Stampa le frequenze di tutti i nodi

--- Prove/ProvaAlberi.cpp
+++ Prove/ProvaAlberi.cpp
@@ -8,12 +8,14 @@
 #define COUNT 10
 #define N 255
 
+//ashdihewqiurhiuehzkjhdiasuhdiuheriuwheiurhw
+
 using namespace std;
 
 typedef struct Nodo{
     int freq;	//Somma dei nodi nel nodo intermedio
     char lett;  //Vale (char)3 nel nodo intermedio
-    int codifica[255];
+    int codifica[N];
     struct Nodo *sx;
     struct Nodo *dx;
 }Nodo;
@@ -38,13 +40,14 @@ Nodo **p = new Nodo *[N];
 
 int main()
 {	
-    int i=0, freq[N], codifica[N], lettere[N];
+    int i=0, j=0, freq[N], codifica[N], lettere[N];
 	char *stringa;
 	for(i=0; i<N; i++)
 	{
 		p[i]=NULL;
 		freq[i]=0;
 		codifica[i]=2;
+		lettere[i]=0;
 	}
 	
 	stringa=readinput();
@@ -62,6 +65,7 @@ int main()
 			p[i]=NULL;
 		}
 	}
+	
 	spostanull();
 	selectionSort();
 	
@@ -70,7 +74,6 @@ int main()
 		if(p[i]!=NULL);
 	}
 
-	
 	while(p[1]!=NULL)
 	{
 		sommanodi(p[0], p[1]);
@@ -84,10 +87,13 @@ int main()
 		{
 			codificacaratteri(p[0], (char)i, 0, codifica);
 			printf("%c -> ", char(i));
-			for(i=0; i<N; i++)
+			for(j=0; j<N; j++)
 			{
-				if(codifica[i]>1)
-					printf("%d", codifica[i]);
+				if(codifica[j]<=1)
+				{
+					printf("%d", codifica[j]);
+					codifica[j]=2;
+				}
 			}
 			printf("\n");
 		}
@@ -146,7 +152,7 @@ void presenza(char* arr, int lett[])
 {
 	int i, j=0;
 	
-	for(i=0; i<N; i++)
+	for(i=0; i<=N; i++)
 	{
 		if(i!=10 && i!=13)
 		{
@@ -158,6 +164,7 @@ void presenza(char* arr, int lett[])
 				}
 				j++;
 			}while(arr[j]!='\0');
+			j=0;
 		}
 	}
 }
@@ -267,11 +274,13 @@ void sommanodi(Nodo* f, Nodo* q)		//Somma i nodi e metti in nodo intermedio
 
 void codificacaratteri(Nodo* f, char lettera, int i, int codi[])
 {
+	int j;
+	
 	if(f!=NULL)
 	{
 		if(f->lett==lettera)
 		{
-			for(int j=0; j<N; j++)
+			for(j=0; j<N; j++)
 			{
 				f->codifica[j]=codi[j];
 			}
@@ -281,6 +290,10 @@ void codificacaratteri(Nodo* f, char lettera, int i, int codi[])
 			if(f->sx->lett==lettera)
 			{
 				codi[i]=0;
+				for(j=0; j<N; j++)
+				{
+					f->codifica[j]=codi[j];
+				}
 			}
 			else
 			{
